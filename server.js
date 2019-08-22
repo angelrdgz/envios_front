74416dfb4d7d091d16cf91404d88ceb9ac3c347d
Angel Garcia Rodriguez <angelrodriguez@ucol.mx>
2019-08-21 21:59:25 -0500

    postinstall

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/envios_front'));
+app.use(express.static(__dirname + '/dist/ship2go'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/envios_front/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ship2go/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
