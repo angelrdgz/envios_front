6abb2efc034f27b19cd6caf060803e0e2befc93d
Angel Garcia Rodriguez <angelrodriguez@ucol.mx>
2019-08-21 21:43:01 -0500

    env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  APIEndpoint: 'https://dev.endpoint.com',
+  srEnvioEndpoint: 'https://api-demo.srenvio.com/v1',
+  srEnvioToken: 'Token token=HCjec5n0TQ2l68riyOMkPH8LkMb3bGRGVafEL5zBZlgt',
 };
