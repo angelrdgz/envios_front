8a1febea50d45f0ba0447915c273fce3d4cfecc6
Angel Garcia Rodriguez <angelrodriguez@ucol.mx>
2019-09-04 19:43:17 -0500

    enviroment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  APIEndpoint: 'http://angel.myvalred.com/public/api',
+  APIEndpoint: 'https://ship2go-api.herokuapp.com/public/api',
   srEnvioEndpoint: 'https://api-demo.srenvio.com/v1',
   srEnvioToken: 'Token token=HCjec5n0TQ2l68riyOMkPH8LkMb3bGRGVafEL5zBZlgt',
 };
