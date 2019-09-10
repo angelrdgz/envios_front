4c1973f3a4a2d35a56c469e5485ae06f43f34b15
Ángel García <angelrodriguez@ucol.mx>
2019-09-10 12:43:25 -0500

    update route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  APIEndpoint: 'http://localhost:8000/api',
+  APIEndpoint: 'https://ship2go-api.herokuapp.com//api',
   srEnvioEndpoint: 'https://api-demo.srenvio.com/v1',
   srEnvioToken: 'Token token=HCjec5n0TQ2l68riyOMkPH8LkMb3bGRGVafEL5zBZlgt',
 };
