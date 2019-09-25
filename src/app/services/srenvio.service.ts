489bff706abf77b02dd86ea6b49d9218cf3d4135
angelrodgz <angelrodriguez@ucol.mx>
2019-09-25 15:14:16 -0500

    Fixes

--- src/app/services/srenvio.service.ts
+++ src/app/services/srenvio.service.ts
@@ -3,14 +3,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 //import { Observable } from 'rxjs/Observable';
 
-const srEnvioEndpoint = environment.srEnvioEndpoint;
-const srEnviToken = environment.srEnvioToken;
-
 const httpOptions = {
   headers: new HttpHeaders(
     { 
-      'Content-Type': 'application/json', 
-      'Authorization':  srEnviToken
+      'Content-Type': 'application/json'
     }
     )
 };
@@ -21,9 +17,5 @@ const httpOptions = {
 export class SrenvioService {
 
   constructor(private http:HttpClient) { }
-
-  quote(data){
-    return this.http.post(srEnvioEndpoint+'/quotations', data, httpOptions);
-  }
 }
 
