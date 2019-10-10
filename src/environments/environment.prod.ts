c943cffd5cd5814ccebea25e66103f7386db1015
Ángel García <angelrodriguez@ucol.mx>
2019-10-09 21:08:44 -0500

    fix route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  APIEndpoint: 'https://ship2go-api.herokuapp.com//api',
+  APIEndpoint: 'https://ship2go-api.herokuapp.com/api',
   srEnvioEndpoint: 'https://api-demo.srenvio.com/v1',
   srEnvioToken: 'Token token=HCjec5n0TQ2l68riyOMkPH8LkMb3bGRGVafEL5zBZlgt',
   enviaEndpoint:'https://api-test.envia.com',
