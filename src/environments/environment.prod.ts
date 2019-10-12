268e23340ec1630601a729af55441c112a96fcae
Ángel García <angelrodriguez@ucol.mx>
2019-10-12 09:48:41 -0500

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  APIEndpoint: 'http://127.0.0.1:8000/api',
+  APIEndpoint: 'https://ship2go-api.herokuapp.com/api',
   srEnvioEndpoint: 'https://api-demo.srenvio.com/v1',
   srEnvioToken: 'Token token=HCjec5n0TQ2l68riyOMkPH8LkMb3bGRGVafEL5zBZlgt',
   enviaEndpoint:'https://api-test.envia.com',
